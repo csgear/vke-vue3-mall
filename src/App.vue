f453863753e058c82508b101fc1dea311affabd4
csgear <csgear@live.com>
2024-03-03 19:07:54 -0500

    feat: MALL-0003 Add element puls component
    In this ticket, I add a form component. Also, it supports auto import.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="form">
+    <Form></Form>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  padding: 1.5em;
-  height: 6em;
-  transition: filter 300ms;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.form {
+  height: 400px;
+  width: 800px;
 }
 </style>
