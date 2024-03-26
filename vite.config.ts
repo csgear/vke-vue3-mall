b2dea26e700172bb6e186c2bcc4c4358470d02bd
YANG XIAOJUN <csgear@live.com>
2024-03-26 16:23:28 -0400

    Csgear/mall 0003 (#1)
    * MALL-0003: MALL-0003 Add element puls component
    
    In this ticket, I add a form component. Also, it supports auto import.
    
    * MALL-0003: add router setup
    
    - add router setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // 把 @ 指向到 src 目录去
     },
   },
   server: {
-    host: true, // host设置为true才可以使用network的形式，以ip访问项目
-    port: 3002, // 端口号
-    open: true, // 自动打开浏览器
-    cors: true, // 跨域设置允许
-    strictPort: true, // 如果端口已占用直接退出
-    // 接口代理
+    host: true,
+    port: 3002,
+    open: true,
+    cors: true,
+    strictPort: true,
     proxy: {
       '/api': {
-        // 本地 8000 前端代码的接口 代理到 8888 的服务端口
         target: 'http://localhost:8888/',
         changeOrigin: true, // 允许跨域
         rewrite: (path) => path.replace('/api/', '/'),
