7403a8dca41449c554c1ca641e007dcd04023bf0
csgear <csgear@live.com>
2024-03-30 12:28:55 -0400

    add api request setttings

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,15 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { reqLogin } from '@/api/user'
+onMounted(() => {
+  reqLogin({
+    username: 'admin',
+    password: '111111',
+  })
+})
+</script>
 
 <style scoped></style>
